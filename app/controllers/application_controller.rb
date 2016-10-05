b82102c042927009ddb72a08988f78e5da41004f
Ed Jones <ed@errorstudio.co.uk>
2016-10-05 10:09:08 +0100

    Stub out rooftop endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,29 @@
 class ApplicationController < ActionController::Base
-      include SimpleErrors::Rescue
+  include SimpleErrors::Rescue
+  rescue_with_not_found Rooftop::RecordNotFoundError, Rooftop::Rails::AncestorMismatch, ActionController::RoutingError
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_rescue do
+    get_navigation_menus
+    get_global_content
+  end
+
   protect_from_forgery with: :exception
+
+  before_action :get_navigation_menus
+  before_action :get_global_content
+  before_action :allow_caching
+
+  private
+  def get_navigation_menus
+    @main_navigation_menu, @footer_navigation_menu = *Rooftop::Menus::Menu.where(post__in: [2,3]).to_a.sort_by(&:id)
+  end
+
+  def get_global_content
+
+  end
+
+  def allow_caching
+    expires_in(10.seconds, public: true)
+  end
 end
