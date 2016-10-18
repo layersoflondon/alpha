fe2171b4cbba9ff759bf6a3d97dad9bea412c046
Paul Hendrick <paul@errorstudio.co.uk>
2016-10-18 18:37:57 +0100

    get a working $.get call hooked up

--- app/assets/javascripts/actions/SearchResultsActions.js.jsx
+++ app/assets/javascripts/actions/SearchResultsActions.js.jsx
@@ -4,22 +4,26 @@
       return (dispatch) => {
         dispatch();
 
-        var self = this;
+        // var self = this;
+        // setTimeout(function() {
+        //   self.emitUpdatedState([{id: 1, name: "This is a newly added pin", position: [51.525, -0.09], places: [{id: 1, name: "Searched Pin, Place 1"}, {id: 2, name: "Searched Pin, Place 2"}]}]);
+        // }, 2000);
 
-        setTimeout(function() {
-          self.emitUpdatedState([{id: 1, name: "This is a newly added pin", position: [51.525, -0.09], places: [{id: 1, name: "Searched Pin, Place 1"}, {id: 2, name: "Searched Pin, Place 2"}]}]);
-        }, 2000);
+        SearchResultsSource.fetch().then((new_state) => {
+          console.log(new_state);
+          this.emitUpdatedState(new_state);
+        }).catch((error) => {
+          console.log("Error!", error);
+        });
+
       }
     }
 
     emitUpdatedState(state) {
-      this.updatePins(state);
-
-      var places = state.map(function(result){return result.places});
-      places = _.flatten(places);
-      places = _.uniqBy(places, function(p){return p.id});
-
-      this.updatePlaces(places);
+      this.updatePins(state.pins);
+      this.updatePlaces(state.places);
+      this.updateOverlays(state.overlays);
+      this.updateCollections(state.collections);
     }
 
     updatePins(pins) {
@@ -36,6 +40,10 @@
 
     updateCollections(collections) {
       return collections;
+    }
+
+    toggleOverlayVisibility(overlay_id) {
+      return overlay_id;
     }
   }
 
