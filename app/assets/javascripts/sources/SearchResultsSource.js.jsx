d475d3e1d20089050de7b822edc9c01e4ed29c2b
Paul Hendrick <paul@errorstudio.co.uk>
2016-10-19 17:51:47 +0100

    Use the new /pins/search endpoint for generating json

--- app/assets/javascripts/sources/SearchResultsSource.js.jsx
+++ app/assets/javascripts/sources/SearchResultsSource.js.jsx
@@ -1,7 +1,17 @@
 var SearchResultsSource = {
   fetch: function() {
     return new Promise(function(resolve, reject) {
-      $.get('/fetch', {search: FilterStateStore.getState()}, function(response) {
+      $.get('/pins/search', {search: FilterStateStore.getState()}, function(response) {
+        resolve(response);
+      }.bind(this));
+    });
+  },
+
+  postPin: function(pin_data) {
+    console.log("SearchResultSource pin_data", pin_data);
+
+    return new Promise(function(resolve, reject) {
+      $.post('/pins', pin_data, function(response){
         resolve(response);
       }.bind(this));
     });
