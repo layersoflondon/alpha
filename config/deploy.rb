aeb5e7f43b6f0f6061bfbf950c3e2f2b32fa0ecf
Ed Jones <ed@errorstudio.co.uk>
2016-10-20 14:10:41 +0100

    Add alias to yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # Basic details
 set :application, 'layers_of_london'
 set :repo_url, 'git@github.com:layersoflondon/application'
-set :primary_domain, "layersoflondon"
+set :primary_domain, "www.layersoflondon.org"
 
 after 'deploy:updated', 'npm:install'
