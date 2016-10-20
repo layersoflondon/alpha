46aea60621c29e6dbefdbb636f0d6aa4f94428cf
Paul Hendrick <paul@errorstudio.co.uk>
2016-10-20 17:03:22 +0100

    Observe the pin_form_enabled MapPinStore state attribute and display a banner when in 'adding a new pin' mode

--- app/assets/javascripts/components/map_container.es6.jsx
+++ app/assets/javascripts/components/map_container.es6.jsx
@@ -2,20 +2,41 @@ class MapContainer extends React.Component {
   constructor(props) {
     super(props);
 
-    self.state = {};
+    this.state = {};
+    this.mapPinStoreChanged  = this.mapPinStoreChanged.bind(this);
+  }
+
+  componentDidMount() {
+    // map state when adding new pins
+    MapPinStore.listen(this.mapPinStoreChanged);
+  }
+
+  componentWillUnmount() {
+    MapPinStore.unlisten(this.mapPinStoreChanged);
+  }
+
+  mapPinStoreChanged(state) {
+    if(state.pin_form_enabled){
+      this.setState({adding_pin: true});
+    }else {
+      this.setState({adding_pin: false});
+    }
   }
 
   render() {
+    var adding_pin = this.state.adding_pin ? (<div className="m-adding-pin-banner">Click on the map to add a pin</div>) : '';
+
     return (
       <div>
         <MapView />
 
         <SideBar />
 
-        <UpdateResultsControl />
         <AddPinControl />
 
         <PinForm />
+
+        {adding_pin}
       </div>
     )
   }
