718ee2b3e19eb12c34393c9b4a2c2b4385c103b5
Paul Hendrick <paul@errorstudio.co.uk>
2016-11-03 16:21:42 +0000

    fix overlay model associations

--- app/models/overlay.rb
+++ app/models/overlay.rb
@@ -1,6 +1,8 @@
 class Overlay < ActiveRecord::Base
   belongs_to :overlay_type
-  belongs_to :overlay_content_entry
+
+  has_one :overlay_content_entry
+  has_one :content_entry, through: :overlay_content_entry
 
   validates :lat, :lng, :title, :date_from, :date_to, presence: true
 end
