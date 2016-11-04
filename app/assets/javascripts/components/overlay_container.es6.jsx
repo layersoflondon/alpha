590643067af38459d1b6b6852fb3804de516c8d1
Paul Hendrick <paul@errorstudio.co.uk>
2016-11-04 12:05:56 +0000

    overlays render a their specific resource type (WIP - only polygon and tile types supported)

--- app/assets/javascripts/components/overlay_container.es6.jsx
+++ app/assets/javascripts/components/overlay_container.es6.jsx
@@ -7,13 +7,23 @@ class OverlayContainer extends React.Component {
     super(props);
 
     this.state = props;
-
-    console.log(this.state);
   }
 
   render () {
-    return (
-      <div />
-    );
+    var component = null;
+
+    switch(this.state.overlay.resource.type) {
+      case 'tile':
+        component = <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url='http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png'></TileLayer>
+        break;
+      case 'polygon':
+        component = <Polygon positions={this.state.overlay.resource.polygon} />;
+        break;
+      case 'data':
+        component = <div />;
+        break;
+    }
+
+    return component;
   }
 }
