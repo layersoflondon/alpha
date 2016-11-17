f23e447cddb25b84730e0112975d23981b4a946a
Ed Jones <ed@errorstudio.co.uk>
2016-11-17 16:05:55 +0000

    refactor pin type picker onClick events

--- app/assets/javascripts/components/pin_form/pin_type_picker.es6.jsx
+++ app/assets/javascripts/components/pin_form/pin_type_picker.es6.jsx
@@ -1,19 +1,23 @@
 class PinTypePicker extends React.Component {
 
-  changePinType(event) {
+  changePinType(event,type) {
     event.preventDefault();
-    MapPinActions.setPinType(event.target.dataset.pinType);
+    MapPinActions.setPinType(type);
+  }
+
+  className(type) {
+    return this.state.pin_type == type ? "selected" : ""
   }
 
   render () {
     return(
         <div className="form-group form-group-pin-type">
           <label>What type of pin are you adding?</label>
-          <a href="#" onClick={this.changePinType.bind(this)} className={this.state.pin_type == "text" ? "selected" : ""} data-pin-type="text"><span>Just text</span></a>
-          <a href="#" onClick={this.changePinType.bind(this)} className={this.state.pin_type == "image" ? "selected" : ""} data-pin-type="image"><span>An image</span></a>
-          <a href="#" onClick={this.changePinType.bind(this)} className={this.state.pin_type == "video" ? "selected" : ""} data-pin-type="video"><span>A video</span></a>
-          <a href="#" onClick={this.changePinType.bind(this)} className={this.state.pin_type == "audio" ? "selected" : ""} data-pin-type="audio"><span>Some audio</span></a>
-          <a href="#" onClick={this.changePinType.bind(this)} className={this.state.pin_type == "dataset" ? "selected" : ""} data-pin-type="dataset"><span>A dataset</span></a>
+          <a href="#" onClick={(event) => {this.changePinType(event,'text')}} className={this.className('text')}><span>Just text</span></a>
+          <a href="#" onClick={(event) => {this.changePinType(event,'image')}} className={this.className('image')}><span>An image</span></a>
+          <a href="#" onClick={(event) => {this.changePinType(event,'video')}} className={this.className('video')}><span>A video</span></a>
+          <a href="#" onClick={(event) => {this.changePinType(event,'audio')}} className={this.className('audio')}><span>Some audio</span></a>
+          <a href="#" onClick={(event) => {this.changePinType(event,'dataset')}} className={this.className('dataset')}><span>A dataset</span></a>
           <Popover title="What's a dataset?">
             You might have data in a structured format about a place - some school attendance registers, census data or similar. If you have, you can upload this as a CSV or TSV file for researchers to use...
           </Popover>
