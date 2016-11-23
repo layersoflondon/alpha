f787637369c879816d9095f9b3eaf1f0349eff42
Paul Hendrick <paul@errorstudio.co.uk>
2016-11-23 16:55:02 +0000

    call .send on the record when checking for presence of any given attributes

--- lib/validators/any_presence_validator.rb
+++ lib/validators/any_presence_validator.rb
@@ -1,6 +1,6 @@
 class AnyPresenceValidator < ActiveModel::Validator
   def validate(record)
-    unless options[:fields].any?{|attr| record[attr].present?}
+    unless options[:fields].any?{|attr| record.send(attr).present?}
       record.errors.add(:base, "You need to include at least one of #{options[:fields].collect(&:to_s).to_sentence(last_word_connector: " or ")}")
     end
   end
