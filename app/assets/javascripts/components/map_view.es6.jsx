00f3ccdb135f3f72f1152edc3ab63d0b46950c8b
Paul Hendrick <paul@errorstudio.co.uk>
2016-11-24 16:54:05 +0000

    display map pin for mouse curser when in 'add pin mode'

--- app/assets/javascripts/components/map_view.es6.jsx
+++ app/assets/javascripts/components/map_view.es6.jsx
@@ -84,11 +84,14 @@ class MapView extends React.Component {
       );
     }.bind(this));
 
-    this.map = <Map center={position} zoom={this.state.zoom} className="m-map" ref='map' onDragEnd={this.handleMoved.bind(this)} onZoomEnd={this.handleZoomed.bind(this)} onClick={this.triggerAddPinDialog.bind(this)}>
+    const mapClasses = MapPinStore.getState().pin_form_enabled ? 'm-map add-pin-mode' : 'm-map';
+
+    this.map = <Map className={mapClasses} center={position} zoom={this.state.zoom} ref='map' onDragEnd={this.handleMoved.bind(this)} onZoomEnd={this.handleZoomed.bind(this)} onClick={this.triggerAddPinDialog.bind(this)}>
                  <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url='http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'></TileLayer>
                  {marker_containers}
                  {overlays}
                </Map>;
+
 
     return (this.map);
   }
