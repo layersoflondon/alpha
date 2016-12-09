c0431800ff697922363fdd74c8c4f4d3ede1a045
Paul Hendrick <paul@errorstudio.co.uk>
2016-12-09 17:50:54 +0000

    pass a prop to specify whether the attachment field is required (if the user has entered some text, we don't require the file)

--- app/assets/javascripts/components/pin_form/pin_image_fields.es6.jsx
+++ app/assets/javascripts/components/pin_form/pin_image_fields.es6.jsx
@@ -3,7 +3,7 @@ class PinImageFields extends React.Component {
   render () {
     return(
       <div className="form-grouping form-grouping--pin-type">
-        <PinAttachmentFields title="Select an image file" field_name="image" validation_failed_message="Please attach an image file" />
+        <PinAttachmentFields title="Select an image file" field_name="image" required={true} validation_failed_message="Please attach an image file" />
       </div>
     )
   }
