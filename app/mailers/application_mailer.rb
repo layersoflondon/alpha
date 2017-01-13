1bb080c069081df385cc3ca498a6d4ec2bc57c65
Paul Hendrick <paul@errorstudio.co.uk>
2017-01-13 10:42:39 +0000

    change mailer address and georeferencer url in template

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "noreply@layersoflondon.org", to: "moderation@layersoflondon.org"
+  default from: "noreply@layersoflondon.org", to: "ed+layersoflondon@error.agency"
   layout 'mailer'
 end
