0049441eaf42cda5148a147b96284d29920563ee
Paul Hendrick <paul@errorstudio.co.uk>
2017-02-03 15:43:59 +0000

    fix permitted text mime-type

--- db/migrate/20170203150437_change_text_content_type_mime_types.rb
+++ db/migrate/20170203150437_change_text_content_type_mime_types.rb
@@ -1,6 +1,6 @@
 class ChangeTextContentTypeMimeTypes < ActiveRecord::Migration
   def change
     text_type = ContentType.where(name: "text").first
-    text_type.update_attribute(:mime_type, "application/*,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,text/plain")
+    text_type.update_attribute(:mime_type, "application/*,application/octet-stream,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,text/plain")
   end
 end
