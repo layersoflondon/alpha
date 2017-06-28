2a13d00ba506972d944286522584deb8c23c939e
Paul Hendrick <paul@errorstudio.co.uk>
2017-06-28 17:09:30 +0100

    fix scope typo

--- app/models/overlay_content_entry.rb
+++ app/models/overlay_content_entry.rb
@@ -2,5 +2,5 @@ class OverlayContentEntry < ActiveRecord::Base
   belongs_to :overlay
   belongs_to :content_entry
 
-  validates :overlay, uniqueness: {scipe: :content_entry, message: "A content entry can only be associated with one overlay"}
+  validates :overlay, uniqueness: {scope: :content_entry, message: "A content entry can only be associated with one overlay"}
 end
