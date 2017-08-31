6038bd56a2371e9ee594c7d7ac435518045c30c1
Ed Jones <ed@errorstudio.co.uk>
2017-08-31 16:47:33 +0100

    remove logger entry for flagging overlays

--- app/controllers/overlays_controller.rb
+++ app/controllers/overlays_controller.rb
@@ -1,6 +1,5 @@
 class OverlaysController < ApplicationController
   def flag
-    Rails.logger.debug("***** flagged overlay #{params[:id]}")
 
     ModerationMailer.georeferenced_overlay_flagged(params[:id]).deliver_later
     render json: {status: "OK"}, status: 200
