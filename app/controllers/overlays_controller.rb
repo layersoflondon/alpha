392cada14cb4ab402126c67234d7889b8a6545c1
Ed Jones <ed@errorstudio.co.uk>
2017-09-01 09:28:20 +0100

    Mark flagged images as flagged

--- app/controllers/overlays_controller.rb
+++ app/controllers/overlays_controller.rb
@@ -1,7 +1,15 @@
 class OverlaysController < ApplicationController
   def flag
+    begin
+      if GeoreferencedImage.find_by(georeferencer_id: params[:id]).try(:flag!)
+        ModerationMailer.georeferenced_overlay_flagged(params[:id]).deliver_later
+      end
 
-    ModerationMailer.georeferenced_overlay_flagged(params[:id]).deliver_later
-    render json: {status: "OK"}, status: 200
+    ensure
+
+
+      render json: {status: "OK"}, status: 200
+    end
+
   end
 end
